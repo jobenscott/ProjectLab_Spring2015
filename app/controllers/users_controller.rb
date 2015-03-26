bf7a80272ec73237f1b210a29089ed75a02a1f14
jobenscott <your.jobenscott@yahoo.com>
2015-03-26 16:25:46 -0600

    added the specifies to index, profile and show in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.all
   end
 
   def profile
+    @user = current_user
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
