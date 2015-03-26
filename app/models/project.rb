4cad7bd367347d83db7caf7ddeb651b892d365e1
jobenscott <your.jobenscott@yahoo.com>
2015-03-26 17:03:53 -0600

    add associations between the project, tasks, and the user

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,9 @@
 class Project < ActiveRecord::Base
-	
 	validates :name, presence: true
 	validates :description, presence: true
 	validate :past_due
+  has_many :tasks, dependent: :destroy
+  belongs_to :user
 
 	def past_due
 		if due_date <= Time.now
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about', as: :about
   get '/contact', to: 'static_pages#contact', as: :contact
 
-  resources :projects 
+  resources :projects
   
 end
