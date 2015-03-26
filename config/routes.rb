a452dd80a8fcfe1e7ea1cfa4058f05a663ca8cfa
jobenscott <your.jobenscott@yahoo.com>
2015-03-26 17:05:53 -0600

    generated a tasks controller with basic CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
+  get 'tasks/new'
+
+  get 'tasks/create'
+
+  get 'tasks/edit'
+
+  get 'tasks/update'
+
+  get 'tasks/destroy'
+
   root 'static_pages#home'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
